d9d36448547a4dfd6719eb9ea103c3725c60fc0e
Jae Yong Kim <kim900180@gmail.com>
2018-07-13 21:50:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,10 +19,17 @@ function pressIt(){
   })
 }
 
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
 getIt()
 frameIt()
+pressIt()
 
 });
